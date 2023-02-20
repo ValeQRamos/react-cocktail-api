742ed677e7f1cb6db42b20163dc0434c7a50ef45
Vale Quiroga <90653297+ValeQRamos@users.noreply.github.com>
2023-02-20 09:37:03 -0600

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> about us </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, aut
+        temporibus molestias nemo officiis illum, dignissimos itaque sit rerum
+        sed ducimus, autem inventore minus quam error. Facilis expedita quam
+        beatae.
+      </p>
+    </section>
+  );
+};
+
+export default About;
